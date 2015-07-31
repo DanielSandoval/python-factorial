4e4a21a403ecabd4fb84f161a636ae9198e6227e
DanielSandoval <danielsandoval182@gmail.com>
2015-07-31 14:00:57 -0600

    Ask number and calculate the factorial

--- application.py
+++ application.py
@@ -1,3 +1,36 @@
 # Aqui escribe tu codigo
 
-print "Hello World"
+import os
+import sys
+
+def factorial(number):
+	if number > 1:
+		number *= factorial(number - 1)
+		return number
+	else:
+		return 1
+
+def verify_number():
+	number = ""
+	while number.isdigit() == False:
+		try:
+			number = raw_input("Enter a number: ")
+			number = int(number)
+			return number
+		except ValueError:
+			print "Enter only numbers\n"
+
+def clean_screen():
+	os.system('reset')
+
+def exit():
+	sys.exit()
+
+def my_application():
+	while True:
+		clean_screen()
+		number = verify_number()
+		print factorial(number)
+		message = raw_input("PRESS ENTER")
+
+my_application()
